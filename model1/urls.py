641ff2232cb590efda337fdaa53865d17fac4034
divya <tiwari.divya50@gmail.com>
2018-05-12 13:36:03 +0530

    Learning Django basics     - built some urls     - used namespacing url names     - used csrf tag

--- model1/urls.py
+++ model1/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'model1'
+
 urlpatterns = [
     path('', views.index, name='index'),
     #path: /model1/1
     path('<int:question_id>/', views.detail, name='detail'),
-    #path: /model1/results
+    #path: /model1/1/results
     path('<int:question_id>/results/', views.results, name='results'),
-    #path: /model1/vote
+    #path: /model1/1/vote
     path('<int:question_id>/votes/', views.vote, name='vote')
 ]
--- model1/views.py
+++ model1/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 
 
 def index(request):
-    latest_question_request = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_request])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:3]
+    template = loader.get_template('model1/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("Detail section %s," % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'model1/detail.html', {'question': question})
 
 
 def results(request, question_id):
